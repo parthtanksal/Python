bc2932251c510895d7ebac138d68e46b5e77b394
parthtanksal <parth112007@gmail.com>
2025-06-18 23:00:27 +0530

    greater number

---
+++ greater.py
@@ -0,0 +1,7 @@
+val1=int(input("Enter first number: "))
+val2=int(input("Enter second number: "))
+print("\n")
+if val1>val2:
+    print(val1," is greater that ",val2)
+else:
+    print(val2," is greater than ",val1)
