aba323be1d619823f746f7fc5046ab7bf1c91b7d
parthtanksal <parth112007@gmail.com>
2025-06-18 23:06:57 +0530

    added the yar of retirement

--- age.py
+++ age.py
@@ -1,7 +1,10 @@
 name=input("Enter your name: ")
 age=int(input("Ener your age: "))
 diff=60-age
+year=2025+diff
 print("\n")
 print("Name: ",name)
 print("Age: ",age)
-print(name," will retire in ",diff," years")
+print("Will retire in: ",year)
+print("\n")
+print("Which means,",name," will retire in",diff," years")
